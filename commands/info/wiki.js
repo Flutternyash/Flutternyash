7281dd3d2d6ed05c5542dace0455334e1e4c1f9f
Ivan Nechaev <dreamsoulfly@gmail.com>
2021-02-18 02:15:04 +0300

    fix search and crush

--- commands/info/wiki.js
+++ commands/info/wiki.js
@@ -7,13 +7,15 @@ module.exports = {
   memberName: "Wiki | Вики",
   description: "Забыл что такое вагина? Я могу напомнить использовав Википедию.",
 
+
+
   async execute(message, args) {
     let img = null;
-    const search = args.join(" ");
+    const search = args.join(" ").replace(/[^0-9A-Za-zА-Яа-яЁё\s+]/g, "");
+    console.log(search);
     if (!search) {
       return message.reply("хорошо, а теперь давай еще раз.\n Но только в этот раз введи что ищешь, даунила.");
     }
-
     await wiki(search, 'ru').then(result => {
       img =  result.thumbnail.source;
     }).catch(err => {
